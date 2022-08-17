2442e9deba182ca1b5f7ab6734f42a81d5e8e348
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-08-17 15:19:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+const multiply = num1 * num2;
+multiply;
+ let random = Math.floor(Math.random()* 10) + 1;
+ random;
+ let num3 = 14;
+ let num4 =10;
+ let mod = num3%num4;
+ mod;
+ let max  = Math.max(2, 3,9, 12, 8, 20, 5.9, 8, 14);
+ max;
